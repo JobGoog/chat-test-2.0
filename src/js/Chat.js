a16ee8f7a833bb4fd58363ef486286371d44bd9d
JobGoog <roro425052@gmail.com>
2025-02-25 16:01:02 +0300

    test

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -8,45 +8,49 @@ export default class Chat {
   }
 
   init() {
-    this.ModalForm.createmodalNickname(); 
-    this.modalNickname = document.querySelector(
-      '[data-widget="modalNickname"]'
-    );
-    this.formNickname = this.modalNickname.querySelector('form'); 
-    this.inputNickname = this.formNickname.querySelector('input'); 
+    this.ModalForm.createmodalNickname();
+    this.modalNickname = document.querySelector('[data-widget="modalNickname"]');
+    this.formNickname = this.modalNickname.querySelector('form');
+    this.inputNickname = this.formNickname.querySelector('input');
 
- 
     const handlerClick = (e) => {
       e.preventDefault();
       this.you = this.inputNickname.value;
-      fetch('https://chat-test-2-0.onrender.com/new-user', {
-        method: 'POST',
-        body: JSON.stringify({ name: `${this.inputNickname.value}` }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
+
+      // Создаём WebSocket-соединение
+      this.ws = new WebSocket('wss://chat-test-2-0.onrender.com/ws');
+
+      // Как только соединение установлено, отправляем запрос на регистрацию
+      this.ws.onopen = () => {
+        this.ws.send(JSON.stringify({
+          type: 'new-user',
+          name: this.inputNickname.value,
+        }));
+      };
+
+      // Обрабатываем ответ на регистрацию
+      const loginHandler = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.type === 'login') {
           if (data.status === 'error') {
             alert(data.message);
           } else if (data.status === 'ok') {
-            this.formNickname.removeEventListener('click', handlerClick); 
-            this.modalNickname.remove(); 
-            this.ModalForm.createmodalChat(); 
-            this.area(); 
-            this.sendMessage(data.user.name); 
+            this.ws.removeEventListener('message', loginHandler);
+            this.modalNickname.remove();
+            this.ModalForm.createmodalChat();
+            this.area();
+            this.sendMessage(data.user.name);
             this.closingPage(data.user.name);
           }
-          return;
-        });
+        }
+      };
+      this.ws.addEventListener('message', loginHandler);
     };
+
     this.formNickname.addEventListener('submit', handlerClick);
   }
 
-
   area() {
-    this.ws = new WebSocket(
-      'wss://chat-test-2-0.onrender.com'
-    );
-
     this.userArea = this.container.querySelector('.modalChat__user');
     this.chatArea = this.container.querySelector('.modalChat__chat');
 
@@ -54,25 +58,20 @@ export default class Chat {
       const data = JSON.parse(e.data);
       this.userContainer = document.querySelectorAll('.user');
 
- 
       if (!data.type) {
-        for (let i = 0; i < this.userContainer.length; i++) {
-          this.userContainer[i].remove();
-        }
+        this.userContainer.forEach((el) => el.remove());
       }
 
-
-      for (let i = 0; i < data.length; i++) {
-        let elem = data[i].name;
+      data.forEach((user) => {
+        let elem = user.name;
         if (elem === this.you) {
           elem = 'YOU';
         }
-        this.userHTML = `<div class = "user">${elem}</div>`;
-        this.userArea.insertAdjacentHTML('beforeEnd', this.userHTML);
-      }
+        const userHTML = `<div class="user">${elem}</div>`;
+        this.userArea.insertAdjacentHTML('beforeEnd', userHTML);
+      });
 
-
-      if (data.user != undefined) {
+      if (data.user !== undefined) {
         if (data.user === this.you) {
           this.chatArea.insertAdjacentHTML(
             'beforeEnd',
@@ -81,36 +80,34 @@ export default class Chat {
         } else {
           this.chatArea.insertAdjacentHTML(
             'beforeEnd',
-            `<p class="chatUser">${data.user}:  ${data.msg}</p>`
+            `<p class="chatUser">${data.user}: ${data.msg}</p>`
           );
         }
       }
     });
   }
 
-
   sendMessage(name) {
-    this.addMessage = this.container.querySelector('[data-id="addMessage"]'); 
-    this.addMessageInput = this.addMessage.querySelector('[data-id="message"]'); 
+    this.addMessage = this.container.querySelector('[data-id="addMessage"]');
+    this.addMessageInput = this.addMessage.querySelector('[data-id="message"]');
     this.addMessage.addEventListener('submit', (e) => {
       e.preventDefault();
-      this.ws.send(
-        JSON.stringify({
-          msg: this.addMessageInput.value,
-          type: 'send',
-          user: name,
-        })
-      );
+      this.ws.send(JSON.stringify({
+        msg: this.addMessageInput.value,
+        type: 'send',
+        user: name,
+      }));
       this.addMessageInput.value = '';
     });
   }
 
-
   closingPage(name) {
     window.addEventListener('unload', () => {
-      this.ws.send(
-        JSON.stringify({ msg: 'вышел', type: 'exit', user: { name } })
-      );
+      this.ws.send(JSON.stringify({
+        msg: 'вышел',
+        type: 'exit',
+        user: { name }
+      }));
     });
   }
 }
